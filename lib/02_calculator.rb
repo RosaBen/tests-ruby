f26041a9451ec50c2f83f53984c429e614f8d6d4
RosaBen <benchabane.rosa@outlook.com>
2025-05-19 14:29:08 +0200

    exo 02 done

---
+++ lib/02_calculator.rb
@@ -0,0 +1,41 @@
+def add (nb1, nb2)
+sumAdd = nb1+nb2
+sumAdd
+end
+
+def subtract(nb1,nb2)
+sub = nb1-nb2
+sub
+end
+
+def sum(array)
+return array.sum
+end
+
+def multiply(nb1,nb2)
+multiplies = nb1*nb2
+multiplies.times do |i|
+  multiplies*i
+end
+return multiplies 
+end
+
+def power (nb1, nb2)
+ return  nb1 ** nb2
+
+end
+
+# https://www.mathsisfun.com/numbers/factorial.html
+def factorial (nb)
+if nb == 0 || nb == 1
+  return fact = 1 
+else
+  fact = 1
+  # à partir de nb va jusqu'à 2 , prends x et fait fact = fact *x
+nb.downto(2) do |x|
+  fact *= x
+end
+return fact
+end
+end
+
